54ef20313f5400f90cdc4574e09e7e33e6266874
boyzcomzretr <ballin.alex@ya.ru>
2026-04-18 18:28:55 +0300

    Initial Commit

---
+++ CasseteCalc+/CasseteCalc_App.swift
@@ -0,0 +1,17 @@
+//
+//  CasseteCalc_App.swift
+//  CasseteCalc+
+//
+//  Created by Александр Малахов on 18.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct CasseteCalc_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
